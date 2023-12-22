cf9326b24a01b275f6a7ab6e9096398a549a59f9
Arne Beduerftig <arne.beduerftig@free-now.com>
2023-12-22 15:40:39 +0100

    Add kapt to maven kotlin plugin

--- src/main/kotlin/com/example/demo/TestController.kt
+++ src/main/kotlin/com/example/demo/TestController.kt
@@ -1,10 +1,10 @@
 package com.example.demo
 
 import com.example.demo.view.ActionViewComponent
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class TestController(
     private val actionViewComponent: ActionViewComponent
 ) {
